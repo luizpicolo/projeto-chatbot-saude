7728b49117c1f124571e9f66f493945523719f85
Luiz Picolo <luizpicolo@gmail.com>
2022-04-25 14:01:01 -0400

    Removido comentáios e adicionado ChatID para função loading_done

--- bot/index.js
+++ bot/index.js
@@ -4,16 +4,13 @@ const ChatBot = require("./model")
 const Telegran = require('../config/telegran')
 const bot = new TelegramBot(Telegran.token, {polling: true});
 
-//const { Usuario } = require('../app/models');
-
 const chatbot = new ChatBot();
 
 bot.on('message', async (msg, match) => {
-  //console.log(await Usuario.findAll());
   console.log("Mensagem" + "\n")
   console.log(JSON.stringify(msg.chat) + "\n\n")
 
   const chatId = msg.chat.id;
-  const resp = await chatbot.loading_done(latinize(msg.text))
+  const resp = await chatbot.loading_done(latinize(msg.text), chatId)
   bot.sendMessage(chatId, resp);
 });
