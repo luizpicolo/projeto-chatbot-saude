ddeba4a4d3cc915e4155815e18a9e7cf3be904db
Luiz Picolo <luizpicolo@gmail.com>
2022-04-25 14:02:22 -0400

    Adicionado UTF8 como encoding e chatDI para função loading_done

--- bot/model/index.js
+++ bot/model/index.js
@@ -1,12 +1,14 @@
 const RiveScript = require('rivescript');
+const { Paciente } = require('../../app/models');
 
 const ChatBot = function() {
   var self = this;
-  self.rs = new RiveScript();
+  self.rs = new RiveScript({utf8: true});
   self.rs.loadDirectory("/app/bot/brain").then(self.loading_done).catch(self.loading_error);
 
-  self.loading_done =  async function(req){
+  self.loading_done = async function(req, chatID){
     self.rs.sortReplies();
+    self.id = chatID;
     return await self.rs.reply('nome', req, self);
   }
 
@@ -16,6 +18,11 @@ const ChatBot = function() {
 
   self.getDataExame = function() {
       return "20/12/2022"
+  };
+
+  self.verificarCadastro = async function() {
+    const paciente = await Paciente.findOne({ where: { identificador: self.id.toString() } });
+    return paciente
   };
 }
 
