bbdd8ef5074826ccb3949272429bd95b23fc11d1
Luiz F. Picolo <luizpicolo@gmail.com>
2022-06-26 23:36:21 -0400

    Removendo modelos desnecessários

--- bot/messenger/telegran.js
+++ bot/messenger/telegran.js
@@ -2,8 +2,6 @@ const TelegramBot = require('node-telegram-bot-api');
 const latinize = require('latinize');
 const ChatBot = require("../models")
 const telegran = require('../../config/tokens')
-const schedule = require('node-schedule');
-const { Agendamento, Paciente } = require('../../app/models');
 const moment = require('moment');
 const bot = new TelegramBot(telegran.token, {polling: true});
 
@@ -19,19 +17,3 @@ exports.start = bot.on('message', async (msg, match) => {
   const resp = await chatbot.loading_done(latinize(msg.text), chatId, 'telegran')
   bot.sendMessage(chatId, resp);
 });
-
-// exports.sendMessage = async () => {
-//     const um_dia_atras = new Date(new Date().setDate(new Date().getDate() - 1));
-//     var paciente = await Paciente.findAll(
-//       {
-//         where: {
-//           my_date: {
-//             $gt: um_dia_atras,
-//             $lt: Agendamento.data_agendamento,
-//           },
-//         },
-//       });
-
-//       bot.sendMessage(chatid, "Olá, seu exame está chegando perto, ele está marcado "+ data_formatada);
-     
-// }
