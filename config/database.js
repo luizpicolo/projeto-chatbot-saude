ae6edeebaff28a7fb720750d6fa39b9c0d5489f9
Luiz F. Picolo <luizpicolo@gmail.com>
2022-07-10 21:45:32 -0400

    Removido parametro adicionar para senhas

--- config/database.js
+++ config/database.js
@@ -41,7 +41,6 @@ module.exports = {
     pkg: process.env.REDIS_PKG,
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD,
     database: process.env.REDIS_DATABASE,
     options: {
       password: process.env.REDIS_PASSWORD
