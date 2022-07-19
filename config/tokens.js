fce79756386661c3ce22395598c75831b5112761
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-07-19 19:24:37 -0300

    Deletado o documento tokens.js

--- config/tokens.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Telegran
-  token: '5313419199:AAEb8HaSB1v7oKZcKNfpcb8Ye9HHa4ag2fA',
-
-  // WhatsApp
-  accountSid: 'ACcdb66abaa9a28546d330a22dc139b81b',
-  authToken: '53ceb590089a68e24d60949b5cd810a3'
-}
